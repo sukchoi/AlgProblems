49180daf6be3b31db7a969e0fb484cc3eec2819d
KeunsukChoi <gschoi92@naver.com>
2022-10-12 21:48:55 +0900

    Temp commit

--- src/programmers42746/Main.java
+++ src/programmers42746/Main.java
@@ -1,1 +1,11 @@
 package programmers42746;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42746
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
