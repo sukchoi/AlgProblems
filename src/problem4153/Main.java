e9d4e14bc4e93bd16174196e1457a4ca899ffceb
sukchoi <gschoi92@naver.com>
2022-12-12 20:06:46 +0900

    12/12 BOJ 4153

--- src/problem4153/Main.java
+++ src/problem4153/Main.java
@@ -13,6 +13,18 @@ public class Main {
       for (int i = 0; i < sides.length; i++) {
         sides[i] = Integer.parseInt(stk.nextToken());
       }
+      if (sides[0] == 0 && sides[1] == 0 && sides[2] == 0) {
+        break;
+      }
+      if (sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2]) {
+        System.out.println("right");
+      } else if (sides[0] * sides[0] + sides[2] * sides[2] == sides[1] * sides[1]) {
+        System.out.println("right");
+      } else if (sides[2] * sides[2] + sides[1] * sides[1] == sides[0] * sides[0]) {
+        System.out.println("right");
+      } else {
+        System.out.println("wrong");
+      }
     }
   }
 }
